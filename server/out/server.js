68f4c5173b0edcbfc2369dce57487e8c6602853a
pamkirsten <69353348+pamkirsten@users.noreply.github.com>
2023-04-10 23:13:18 +0800

    Update server.js

--- server/out/server.js
+++ server/out/server.js
@@ -164,7 +164,7 @@ documents.onDidClose((e) => {
 });
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
-documents.onDidChangeContent((change) => {
+documents.onDidSave((change) => {
   validateTextDocument(change.document);
 });
 
